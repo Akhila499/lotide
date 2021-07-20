fa5d817cabc2dfd2f3b7f348c75a8743c5d23ecf
Akhila499 <akhilathota499@gmail.com>
2021-07-19 20:40:02 -0400

    used template strings or template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ const assertEqual = function(actual, expected) {
   let emojiFail = String.fromCodePoint(0x1F534);
   let emojiPass = String.fromCodePoint(0x1F7E2);
   if (actual === expected) {
-    console.log(emojiPass + ' Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(emojiFail + ' Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`${emojiFail} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
