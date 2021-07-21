bd41a9c19beea34d44505004f4cf77bb3b41e1de
Akhila499 <akhilathota499@gmail.com>
2021-07-21 18:07:10 -0400

    made some changes

--- countLetters.js
+++ countLetters.js
@@ -1,8 +1,9 @@
 const assertEqual = function(actual, expected) {
   let emojiFail = String.fromCodePoint(0x1F534);
   let emojiPass = String.fromCodePoint(0x1F7E2);
-  if (actual === expected) {
-    console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
+  //console.log(actual,expected);
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`${emojiPass} Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
   } else {
     console.log(`${emojiFail} Assertion Failed: ${actual} !== ${expected}`);
   }
@@ -11,17 +12,16 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(str) {
 
   let charCount = {};
-  for(let char of str){
+  for (let char of str) {
     //console.log(char);
-    if(charCount[char]){
+    if (charCount[char]) {
       charCount[char] += 1;
-    }else{
+    } else {
       charCount[char] = 1;
     }
   }
   delete charCount[' '];
-  console.log(charCount);
+  //console.log(charCount);
   return charCount;
-};        
-
-countLetters('lighthouse in the house');
+};
+assertEqual(countLetters('lighthouse in the house'),{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
