d671accdfb2e80ca8ac80510d1b6b5536be22281
Akhila499 <akhilathota499@gmail.com>
2021-07-21 19:57:14 -0400

    created a program which can find the key for a given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  let emojiFail = String.fromCodePoint(0x1F534);
+  let emojiPass = String.fromCodePoint(0x1F7E2);
+  if (actual === expected) {
+    console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emojiFail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj,value){
+  arr = Object.keys(obj);
+  
+  for(let val of arr){
+   
+    if (obj[val] === value){
+      console.log(val);
+      return val;
+    }
+   
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
