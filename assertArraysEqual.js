5f5e8815767a19e0244cd36d958037c9074563d8
Akhila499 <akhilathota499@gmail.com>
2021-07-20 21:03:25 -0400

    made some changes.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,9 +8,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   
-  if (array1.length !== array2.length) {
+  if(array1.length !== array2.length) {
     
     return false;
 
@@ -18,7 +18,7 @@ const eqArrays = function(array1, array2) {
 
   for (let i = 0; i < array1.length; i++) {
     
-    if (array1[i] !== array2[i]) {
+    if(array1[i] !== array2[i]){
       return false;
     }
     
@@ -27,6 +27,7 @@ const eqArrays = function(array1, array2) {
   return true;
   
 };
+
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
