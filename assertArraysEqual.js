e4be50d7d90c8d44d798d9beb7bfe8f77d67fe66
Akhila499 <akhilathota499@gmail.com>
2021-07-20 21:22:48 -0400

    created assertArraysEqual function and checked for array comparision.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,29 @@
-const assertEqual = function(actual, expected) {
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  
   let emojiFail = String.fromCodePoint(0x1F534);
   let emojiPass = String.fromCodePoint(0x1F7E2);
-  if (actual === expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`${emojiFail} Assertion Failed: ${actual} !== ${expected}`);
   }
+
 };
 
-const eqArrays = function (array1, array2) {
-  
-  if(array1.length !== array2.length) {
-    
-    return false;
-
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    
-    if(array1[i] !== array2[i]){
-      return false;
-    }
-    
-    
-  }
-  return true;
-  
-};
+assertArraysEqual([1,2,3],[1,2,3]);
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
