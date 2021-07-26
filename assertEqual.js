795c9ef361beed93c423624e6290bf693e68ffc2
Akhila499 <akhilathota499@gmail.com>
2021-07-26 15:04:06 -0400

    Used export and require module and created separate file for headTest and assertEqual test

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Hello","Hello");
-assertEqual(2,3);
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let emojiFail = String.fromCodePoint(0x1F534);
-  let emojiPass = String.fromCodePoint(0x1F7E2);
-  if (actual === expected) {
-    console.log(`${emojiPass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${emojiFail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 
@@ -14,7 +6,6 @@ const head = function (array){
   return array[0];
 }
 
-assertEqual(head([1,2,3]), 1);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]),1);
-assertEqual(head([]), undefined);
+
+module.exports = head ;
+
