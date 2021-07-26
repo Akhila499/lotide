de92c4bc1e1f8b31f64395caf76163dceaac6d06
Akhila499 <akhilathota499@gmail.com>
2021-07-26 18:51:05 -0400

    modified the middleTest file using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle'); 
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle'); 
+// const eqArrays = require('../eqArrays');
 
-console.log(middle([1,2,3,4]));
+// console.log(middle([1,2,3,4]));
 
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-eqArrays(middle([1,2,3]),[2]);
+// assertArraysEqual(middle([1,2,3,4]),[2,3]);
+// eqArrays(middle([1,2,3]),[2]);
+
+const middle = require('../middle');
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it('returns [2,3] when input is [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it('returns [2] when input is [1,2,3]', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+});
