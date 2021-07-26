308b85cebe43bcc2c41893da7dbc757c8fb473da
Akhila499 <akhilathota499@gmail.com>
2021-07-26 19:18:05 -0400

    changes made to use property value shorthand syntax

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
   eqArrays: eqArrays
 };
