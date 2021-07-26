752e408425b71ac7767afc8224ecadf36eaee401
Akhila499 <akhilathota499@gmail.com>
2021-07-26 18:43:17 -0400

    Modified tailTest file using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,22 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+
+  // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+// assertEqual(res[0], "Lighthouse");
+// assertEqual(res[1], "Labs");
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-let res = tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-assertEqual(res[0], "Lighthouse");
-assertEqual(res[1], "Labs");
+let res = tail(words);
+
+describe("#tail", () => {
+  it("returns 'lighthouse' when res[0]", () => {
+    assert.strictEqual(res[0], 'Lighthouse');
+  });
+  it("return 'Labs' when asked res[1]", () => {
+    assert.deepEqual(res,['Lighthouse', 'Labs']);
+  });
+});
